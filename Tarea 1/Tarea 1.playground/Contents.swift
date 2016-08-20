f87f80fcdf1a984d1a602d1e0f12cb6312db8493
Ernesto Cervantes <netocervantes@macbook-pro-de-ernesto.local>
2016-08-19 20:52:47 -0500

    Actualizada

--- Tarea 1/Tarea 1.playground/Contents.swift
+++ Tarea 1/Tarea 1.playground/Contents.swift
@@ -13,13 +13,15 @@ for x in 0...100{
     
         var b = x%2
     if b == 0{
-        print (x, "Par")
+        print (x, "Par")    //Aqui se busca saber si el valor de x es par
     }else if b != 0{
-        print (x, "Impar")
+        print (x, "Impar")  //Aqui se busca saber si el valor de x es impar
     }
     
     
     if x >= 30 && x <= 40{
-        print(x, "Viva Swift!!!")
+        print(x, "Viva Swift!!!")   //Aqui se busca saber si los valores de x estan en el rango
     }
 }
+
+/*Deje todas las condiciones que cada número cumplía porque considere que cada dígito puede cumplir dos condiciones, es por eso que algunos resultados se repiten. Por ejemplo el número 5 es: impar y además múltiplo de cinco por eso también imprime Bingo!!! */
